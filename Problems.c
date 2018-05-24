4884b7b3b90c0a7100ab696b0d4e2d6abc4e91b2
Atabekov Farrukh <32008802+Farrukhbek@users.noreply.github.com>
2018-05-24 15:26:34 +0900

    Create Problems.c

---
+++ Problems.c
@@ -0,0 +1,36 @@
+The solution to the Problem10 (on the functions section) might not be exactly correct but it displays all the matches until the first digits becomes 3
+
+Arrays.c (Problems)
+3
+Problem 1:
+
+Example
+Input
+Input 10 elements in array: 1 2 3 4 5 6 7 8 9 10
+Input number of times to rotate: 3
+Output
+Array after left rotation 3 times: 4 5 6 7 8 9 10 1 2 3
+
+Problem 2:
+C program count total duplicate elements in array
+Write a C program to input elements in array from user and count duplicate elements in array. C program to find all duplicate elements in an unsorted array. How to count duplicate elements in array using loop in C programming.
+
+Example
+Input
+Input array elements: 1, 10, 20, 1, 25, 1, 10, 30, 25, 1
+Output
+Total number of duplicate elements = 5
+
+Problem 3:
+C program to count frequency of each element in an array
+Write a C program to input elements in array and find frequency of each element in array. How to count occurrence of each element in array in C programming using loop. Logic to count frequency of each element in array in C program.
+ 
+Example
+Input
+Input array elements: 5 10 2 5 50 5 10 1 2 2
+Output
+Frequency of 5 = 3
+Frequency of 10 = 2
+Frequency of 2 = 3
+Frequency of 50 = 1
+Frequency of 1 = 1
